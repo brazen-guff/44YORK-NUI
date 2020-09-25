8f87eb2e83874a5bbfd117b5609ef41aabfb0b23
Paul Harding <paul.harding@york.ac.uk>
2020-09-25 14:54:41 +0100

    Update custom.js
    Repositioning of BDS button

--- js/custom.js
+++ js/custom.js
@@ -77,7 +77,9 @@
 		   // controller: 'prmSearchResultAvailabilityLineAfterController'
 		   //});
 		
-		app.component('prmSearchResultAvailabilityLineAfter', {
+		
+			
+	app.component('prmSearchResultAvailabilityLineAfter', {
 			bindings: { parentCtrl: '<',
 				buttonText: '@',
 				buttonLink: '@',
@@ -85,39 +87,15 @@
 				Requestable: '@',
 				Show: '@'},
 			controller: 'prmSearchResultAvailabilityLineAfterController',
+			
+			template: '\n <div ng-If="$ctrl.ShowReqLink && $ctrl.Requestable" class="bar filter-bar layout-align-center-center layout-row margin-top-medium" layout="row" layout-align="center center">\n          <span class="margin-right-small"></span>\n          <a ng-href="{{$ctrl.formURL}}" target="_blank">\n              <button class="button-with-icon zero-margin md-button md-button-raised md-primoExplore-theme" type="button" aria-label={{$ctrl.buttonText}} style="color: #00546E;">\n                  <prm-icon icon-type="svg" svg-icon-set="action" icon-definition="ic_description_24px"></prm-icon>\n                  <span style="text-transform: none;">{{$ctrl.serviceText}}</span>\n              </button>\n          </a>\n      </div>'	
 
 		});
-
-		app.controller('prmSearchResultAvailabilityLineAfterController', function ($scope) {
-
+		
+		
+		app.controller('prmSearchResultAvailabilityLineAfterController', function ($scope, $rootScope, $location) {
+			
 			window.browzine.primo.searchResult($scope);
-		
-			}
-		);
-		
-		
-		
-		//***************************************move book takeaway****************************//
-		
-		//configure book takeaway button
-		//app.component('prmBriefResultContainerAfter', {
-			
-		app.component('prmBriefResultContainerAfter',{
-			bindings: { parentCtrl: '<',
-				buttonText: '@',
-				buttonLink: '@',
-				formURL: '@' ,
-				Requestable: '@',
-				Show: '@'},
-			controller: 'prmBriefResultContainerAfterController',
-			
-			template: '\n <div ng-If="$ctrl.ShowReqLink && $ctrl.Requestable" class="bar filter-bar layout-align-center-center layout-row margin-top-medium" layout="row" layout-align="center center">\n          <span class="margin-right-small"></span>\n          <a ng-href="{{$ctrl.formURL}}" target="_blank">\n              <button class="button-with-icon zero-margin md-button md-button-raised md-primoExplore-theme" type="button" aria-label={{$ctrl.buttonText}} style="color: #00546E;">\n                  <prm-icon icon-type="svg" svg-icon-set="action" icon-definition="ic_description_24px"></prm-icon>\n                  <span style="text-transform: none;">{{$ctrl.serviceText}}</span>\n              </button>\n          </a>\n      </div>'
-			
-
-		});
-		
-		
-		app.controller('prmBriefResultContainerAfterController', function ($scope, $rootScope, $location) {
 
 			//test whether item is available in physical form - any electronic delcategory means that 
 			//the request link shouldn't appear
@@ -147,7 +125,7 @@
 				
 				//is the delivery category in the list defined as non-requestable?
 				var delcat = conditions.some(function (el) {
-					return vm.parentCtrl.item.delivery.deliveryCategory.includes(el);
+					return vm.parentCtrl.result.delivery.deliveryCategory.includes(el);
 				});
 		
 				//YML changes - remove 44YORK_YML_LIB from array of non-requestable libraries
@@ -158,13 +136,13 @@
 					var libCodes = ["44YORK_RBL_LIB", "44YORK_EXST_LIB","44YORK_EXST-B_LIB","44YORK_BIA_LIB","44YORK_NRM_LIB","44YORK_PET_LIB","44YORK_SOF_LIB","44YORK_ACA_LIB"]		
 									
 									
-					var itemLib = vm.parentCtrl.item.delivery.bestlocation.libraryCode;
+					var itemLib = vm.parentCtrl.result.delivery.bestlocation.libraryCode;
 
 					//is our current sub location in the non-requestable list?
 					var rqst = libCodes.indexOf(itemLib);
 					
 					if (itemLib == '44YORK_YML_LIB'){
-						var subLocCode = vm.parentCtrl.item.delivery.bestlocation.subLocationCode;
+						var subLocCode = vm.parentCtrl.result.delivery.bestlocation.subLocationCode;
 						if (subLocCode == 'YM'){
 							console.log ('*************requestable Minster***************');
 							rqst = '-1';
@@ -189,50 +167,50 @@
 						
 						if (vm.displayMode){ //overlay
 											
-							var rec_id = vm.parentCtrl.item.pnx.control.sourcerecordid[0];
+							var rec_id = vm.parentCtrl.result.pnx.control.sourcerecordid[0];
 																	
-							var title = encodeURIComponent(vm.parentCtrl.item.pnx.display.title[0]);
+							var title = encodeURIComponent(vm.parentCtrl.result.pnx.display.title[0]);
 							//entry.1752528148=Title
 
-							var author = encodeURIComponent(vm.parentCtrl.item.pnx.display.creator);
+							var author = encodeURIComponent(vm.parentCtrl.result.pnx.display.creator);
 							//&entry.1866861278=Author
 
-							var material_type = encodeURIComponent(vm.parentCtrl.item.pnx.addata.format);
+							var material_type = encodeURIComponent(vm.parentCtrl.result.pnx.addata.format);
 							//&entry.1859840384=Material+type
 
-							if (vm.parentCtrl.item.pnx.addata.hasOwnProperty('risdate')){
-								var pub_year = encodeURIComponent(vm.parentCtrl.item.pnx.addata.risdate[0]);
+							if (vm.parentCtrl.result.pnx.addata.hasOwnProperty('risdate')){
+								var pub_year = encodeURIComponent(vm.parentCtrl.result.pnx.addata.risdate[0]);
 							}
 
-							var loc = encodeURIComponent(vm.parentCtrl.item.delivery.bestlocation.mainLocation) + ' ' + encodeURIComponent(vm.parentCtrl.item.delivery.bestlocation.subLocation);
+							var loc = encodeURIComponent(vm.parentCtrl.result.delivery.bestlocation.mainLocation) + ' ' + encodeURIComponent(vm.parentCtrl.result.delivery.bestlocation.subLocation);
 
-							var shelfmark = vm.parentCtrl.item.delivery.bestlocation.callNumber;
+							var shelfmark = vm.parentCtrl.result.delivery.bestlocation.callNumber;
 							//&entry.1777930827=Shelfmark
 							shelfmark = encodeURIComponent(shelfmark.replace('&nbsp;&nbsp;', ''));
 						}else{
 						//not in overlay	
 														
-							var rec_id = vm.parentCtrl.item.pnx.control.sourcerecordid[0];
+							var rec_id = vm.parentCtrl.result.pnx.control.sourcerecordid[0];
 							
 											
-							var title = encodeURIComponent(vm.parentCtrl.item.pnx.display.title[0]);
+							var title = encodeURIComponent(vm.parentCtrl.result.pnx.display.title[0]);
 							//entry.1752528148=Title
 
-							var author = encodeURIComponent(vm.parentCtrl.item.pnx.display.creator);
+							var author = encodeURIComponent(vm.parentCtrl.result.pnx.display.creator);
 							//&entry.1866861278=Author
 
-							var material_type = encodeURIComponent(vm.parentCtrl.item.pnx.addata.format);
+							var material_type = encodeURIComponent(vm.parentCtrl.result.pnx.addata.format);
 							//&entry.1859840384=Material+type
 
 							//journal records might not have this field
 							
-							if (vm.parentCtrl.item.pnx.addata.hasOwnProperty('risdate')){
-								var pub_year = encodeURIComponent(vm.parentCtrl.item.pnx.addata.risdate[0]);
+							if (vm.parentCtrl.result.pnx.addata.hasOwnProperty('risdate')){
+								var pub_year = encodeURIComponent(vm.parentCtrl.result.pnx.addata.risdate[0]);
 							}
 							
-							var loc = encodeURIComponent(vm.parentCtrl.item.delivery.bestlocation.mainLocation) + ' ' + encodeURIComponent(vm.parentCtrl.item.delivery.bestlocation.subLocation);
+							var loc = encodeURIComponent(vm.parentCtrl.result.delivery.bestlocation.mainLocation) + ' ' + encodeURIComponent(vm.parentCtrl.result.delivery.bestlocation.subLocation);
 
-							var shelfmark = vm.parentCtrl.item.delivery.bestlocation.callNumber;
+							var shelfmark = vm.parentCtrl.result.delivery.bestlocation.callNumber;
 							//&entry.1777930827=Shelfmark
 							shelfmark = encodeURIComponent(shelfmark.replace('&nbsp;&nbsp;', ''));
 							
